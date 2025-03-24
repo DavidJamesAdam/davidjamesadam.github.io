6988c892384b837d6fdfc3f10da4e4bf86a030ed
DavidAGigadatSolutions <168763102+DavidAGigadatSolutions@users.noreply.github.com>
2025-03-24 11:24:05 -0600

    Updating to QA badges

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <head>
         {/* For inline script information: https://nextjs.org/docs/messages/inline-script-id */}
         <Script id="TrulyBadgeScript">
-          {/* {`(function () {
+          {`(function () {
         var script = document.createElement('script');
 
         script.src = 'https://kmgu6d1qxk.execute-api.us-east-2.amazonaws.com/test/api/tlv1?siteId=93a7a8b1-0c21-47be-9ee0-17dc2fb238bc';
@@ -37,9 +37,9 @@ export default function RootLayout({
 
         var entry = document.getElementsByTagName('script')[0];
         entry.parentNode.insertBefore(script, entry);
-    })();`} */}
+    })();`}
           {/*Prod Badges*/}
-          {`(function () { 
+          {/* {`(function () { 
           var script = document.createElement('script'); 
           
           script.src = 'https://badge.trulylegit.com/api/tlv1?siteId=a3466eb0-fcb7-4d3a-a1ff-4b52260241ad'; 
@@ -47,7 +47,7 @@ export default function RootLayout({
           
           var entry = document.getElementsByTagName('script')[0]; 
           entry.parentNode.insertBefore(script, entry); 
-          })();`}
+          })();`} */}
           {/*Dev Badges*/}
           {/* {`(function () { 
           var script = document.createElement('script'); 
