9b0210bec519ddd4c2df22920e9d39f30f04be3c
DavidJamesAdam <davidjamesadam@gmail.com>
2025-08-09 18:39:40 +0200

    Nav links now evenly spaced

--- src/app/components/navBar.tsx
+++ src/app/components/navBar.tsx
@@ -5,8 +5,8 @@ const Navbar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-dark bg-opacity-75 text-light">
-        <div className="container">
-          <Link className="navbar-brand text-light font-bold" href="/">
+        <div className="container flex justify-evenly w-full px-8 py-4">
+          <Link href="/" className="navbar-brand text-light font-bold w-24">
             Home
           </Link>
           <Link href="/projects" className="nav-item nav-link text-light">
