cf8d9ed6b30ca27fb717d3b17c7d468543785792
DavidJamesAdam <davidjamesadam@gmail.com>
2025-12-25 15:16:24 -0700

    feature: added github links to projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
 export default function Page() {
   return (
     <div className="text-center">
-      <p>Projects</p>
+      <h1>Projects</h1>
+      <div className="projects">
+        <Link href="https://github.com/DavidJamesAdam/cpsc513-term-project-puppli">Puppli - Dog picture voting webapp</Link><br/>
+        <Link href="https://github.com/DavidJamesAdam/RED-Retro-Entertainment-Device-">RED - Retro Entertainment Device</Link><br/>
+        <Link href="https://github.com/DavidJamesAdam/HIC---The-Hiker-s-Information-Companion">HIC - Hikers Information Companion</Link><br/>
+        <Link href="https://github.com/DavidJamesAdam/CalgaryHacks-24">CSUS Calgary Hacks 2024 Hackathon</Link><br/>
+        <Link href="https://github.com/DavidJamesAdam/myFirstServer">Personal Backend Server (CRUD) project</Link>
+      </div>
     </div>
   );
 }
