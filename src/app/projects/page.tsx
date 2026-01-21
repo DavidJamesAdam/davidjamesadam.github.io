cc16a631f0ab57475450ed8fb5ce364e07ccd014
DavidJamesAdam <davidjamesadam@gmail.com>
2026-01-20 17:59:26 -0700

    feature: added cards to project links

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,56 @@
-import Link from "next/link";
+import Card from "@mui/material/Card";
+import { Link } from "@mui/material";
+
+const cardStyle = {
+  width: "50%",
+  height: "50%",
+};
+
 export default function Page() {
   return (
     <div className="text-center">
       <h1>Projects</h1>
-      <div className="projects">
-        <Link href="https://github.com/DavidJamesAdam/cpsc513-term-project-puppli">Puppli - Dog picture voting webapp</Link><br/>
-        <Link href="https://github.com/DavidJamesAdam/RED-Retro-Entertainment-Device-">RED - Retro Entertainment Device</Link><br/>
-        <Link href="https://github.com/DavidJamesAdam/HIC---The-Hiker-s-Information-Companion">HIC - Hikers Information Companion</Link><br/>
-        <Link href="https://github.com/DavidJamesAdam/CalgaryHacks-24">CSUS Calgary Hacks 2024 Hackathon</Link><br/>
-        <Link href="https://github.com/DavidJamesAdam/myFirstServer">Personal Backend Server (CRUD) project</Link>
-        <Link href="https://github.com/DavidJamesAdam/family-history">Family History website</Link>
+      <div
+        className="projects"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyItems: "center",
+        }}
+      >
+        <Card variant="outlined" sx={cardStyle}>
+          <Link href="https://github.com/DavidJamesAdam/cpsc513-term-project-puppli">
+            Puppli - Dog picture voting webapp
+          </Link>
+        </Card>
+
+        <Card variant="outlined" sx={cardStyle}>
+          <Link href="https://github.com/DavidJamesAdam/RED-Retro-Entertainment-Device-">
+            RED - Retro Entertainment Device
+          </Link>
+        </Card>
+
+        <Card variant="outlined" sx={cardStyle}>
+          <Link href="https://github.com/DavidJamesAdam/HIC---The-Hiker-s-Information-Companion">
+            HIC - Hikers Information Companion
+          </Link>
+        </Card>
+
+        <Card variant="outlined" sx={cardStyle}>
+          <Link href="https://github.com/DavidJamesAdam/CalgaryHacks-24">
+            CSUS Calgary Hacks 2024 Hackathon
+          </Link>
+        </Card>
+        <Card variant="outlined" sx={cardStyle}>
+          <Link href="https://github.com/DavidJamesAdam/myFirstServer">
+            Personal Backend Server (CRUD) project
+          </Link>
+        </Card>
+        <Card variant="outlined" sx={cardStyle}>
+          <Link href="https://github.com/DavidJamesAdam/family-history">
+            Family History website
+          </Link>
+        </Card>
       </div>
     </div>
   );
