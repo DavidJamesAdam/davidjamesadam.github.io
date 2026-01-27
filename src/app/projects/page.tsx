f1ed1b89bff881580bb1eb380d71e604fb081b2a
DavidJamesAdam <davidjamesadam@gmail.com>
2026-01-27 13:16:12 -0700

    fix: commented out buggy code so I can deploy properly

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,10 @@
-import Card from "@mui/material/Card";
+// import Card from "@mui/material/Card";
 import { Link } from "@mui/material";
 
-const cardStyle = {
-  width: "50%",
-  height: "50%",
-};
+// const cardStyle = {
+//   width: "50%",
+//   height: "50%",
+// };
 
 export default function Page() {
   return (
@@ -18,39 +18,41 @@ export default function Page() {
           justifyItems: "center",
         }}
       >
-        <Card variant="outlined" sx={cardStyle}>
+        {/* <Card variant="outlined" sx={cardStyle}> */}
           <Link href="https://github.com/DavidJamesAdam/cpsc513-term-project-puppli">
             Puppli - Dog picture voting webapp
           </Link>
-        </Card>
+        {/* </Card> */}
 
-        <Card variant="outlined" sx={cardStyle}>
+        {/* <Card variant="outlined" sx={cardStyle}> */}
           <Link href="https://github.com/DavidJamesAdam/RED-Retro-Entertainment-Device-">
             RED - Retro Entertainment Device
           </Link>
-        </Card>
+        {/* </Card> */}
 
-        <Card variant="outlined" sx={cardStyle}>
+        {/* <Card variant="outlined" sx={cardStyle}> */}
           <Link href="https://github.com/DavidJamesAdam/HIC---The-Hiker-s-Information-Companion">
             HIC - Hikers Information Companion
           </Link>
-        </Card>
+        {/* </Card> */}
 
-        <Card variant="outlined" sx={cardStyle}>
+        {/* <Card variant="outlined" sx={cardStyle}> */}
           <Link href="https://github.com/DavidJamesAdam/CalgaryHacks-24">
             CSUS Calgary Hacks 2024 Hackathon
           </Link>
-        </Card>
-        <Card variant="outlined" sx={cardStyle}>
+        {/* </Card> */}
+
+        {/* <Card variant="outlined" sx={cardStyle}> */}
           <Link href="https://github.com/DavidJamesAdam/myFirstServer">
             Personal Backend Server (CRUD) project
           </Link>
-        </Card>
-        <Card variant="outlined" sx={cardStyle}>
+        {/* </Card> */}
+
+        {/* <Card variant="outlined" sx={cardStyle}> */}
           <Link href="https://github.com/DavidJamesAdam/family-history">
             Family History website
           </Link>
-        </Card>
+        {/* </Card> */}
       </div>
     </div>
   );
