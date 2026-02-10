7a1676c906599878e51ef0aa74b5837935194cc4
DavidJamesAdam <davidjamesadam@gmail.com>
2026-02-10 16:08:17 -0700

    feature: continued to build out the layout of the page

--- src/app/cpsc-581/page.tsx
+++ src/app/cpsc-581/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import DavidButton from "../components/cpsc581-button/david-button";
+import "./styles.css";
 
 export default function Page() {
   return (
-    <div className="bg-[#8DA779] h-screen w-screen">
-      <div className="justify-items-center">
+    <div className="bg-[#8DA779] w-screen">
+      <div className="justify-items-center mb-[10]">
         <Image
           src="/welcome.svg"
-          alt="Profile Picture"
+          alt="Welcome image"
           style={{
             objectFit: "cover",
             borderRadius: "100px",
@@ -16,9 +17,47 @@ export default function Page() {
           height={38}
         />
       </div>
-      <div className="mx-40">
-        <h1>Group Project 1</h1>
-        <div className="justify-items-start">
+      <div className="mx-[300] mb-[100]">
+        <p className="splash-regular text-5xl mb-[20]">
+          Initial Portfolio page design
+        </p>
+        <Image
+          className="mb-[20]"
+          src="/font_activity.jpg"
+          alt="Results of the font activity"
+          style={{
+            objectFit: "cover",
+            borderRadius: "10px",
+          }}
+          width={500}
+          height={38}
+        />
+        <Image
+          className="mb-[20]"
+          src="/portfolio_activity.jpg"
+          alt="Results of portfolio design activity"
+          style={{
+            objectFit: "cover",
+            borderRadius: "10px",
+          }}
+          width={500}
+          height={38}
+        />
+      </div>
+      <div className="mx-[300] justify-items-end">
+        <p className="splash-regular text-5xl  mb-[20]">Group Project 1</p>
+        <Image
+          className="mb-[20]"
+          src="/Group_project_1.png"
+          alt="Profile Picture"
+          style={{
+            objectFit: "cover",
+            borderRadius: "10px",
+          }}
+          width={500}
+          height={38}
+        />
+        <div>
           <DavidButton />
         </div>
       </div>
