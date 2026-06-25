595f6f86060519b54ac97f1c1864d7715d5afcaa
DavidJamesAdam <davidjamesadam@gmail.com>
2026-06-25 11:01:20 -0600

    Revert "Decoupled typing animation"
    This reverts commit a1beb213a41974359c1899326abbc909db169209.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,44 @@
 "use client";
 
-import { useEffect, useMemo, useRef, useState } from "react";
-import { useTypewriter } from "@/utils/useTypewriter";
+import { useEffect, useRef, useState } from "react";
+
+const words = ["Enter the Dreamquest"];
+
+function useTypewriter(texts: string[], period = 2000) {
+  const [text, setText] = useState("");
+  const textRef = useRef("");
+  const loopNum = useRef(0);
+
+  useEffect(() => {
+    let timeoutId: number;
+
+    const tick = () => {
+      const i = loopNum.current % texts.length;
+      const fullText = texts[i];
+      const currentText = textRef.current;
+
+      const updatedText = fullText.substring(0, currentText.length + 1);
+
+      textRef.current = updatedText;
+      setText(updatedText);
+
+      const delay = 200 - Math.random() * 100;
+
+      timeoutId = window.setTimeout(tick, delay);
+    };
+
+    timeoutId = window.setTimeout(tick, 500);
+    return () => clearTimeout(timeoutId);
+  }, [period, texts]);
+
+  return text;
+}
 
 export default function Page() {
-  const sentence = useMemo(() => "Enter the Dreamquest", []);
-  const { typedText, isDone: isTypingDone } = useTypewriter(sentence, {
-    startDelay: 500,
-    minDelay: 100,
-    maxDelay: 200,
-  });
+  const typedText = useTypewriter(words, 2000);
   const [converted, setConverted] = useState<boolean[]>([]);
   const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
+  const isTypingDone = typedText.length === words[0].length;
   const isMobileRef = useRef(false);
   const prevLength = useRef(0);
 
@@ -102,7 +129,7 @@ export default function Page() {
         onMouseLeave={startLeaveAnimation}
         className={
           !isTypingDone
-            ? "pointer-events-none inline-block text-4xl wrap typewriter"
+            ? "pointer-events-none inline-block text-4xl wrap"
             : "inline-block text-4xl wrap typewriter"
         }
       >
