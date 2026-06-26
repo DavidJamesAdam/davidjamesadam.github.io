e0f6b5ffa01b17be259ffa81a85fd7917e316d6c
DavidJamesAdam <davidjamesadam@gmail.com>
2026-06-26 16:37:37 -0600

    Removed dead code

--- src/app/components/terminal-window/TerminalWindow.tsx
+++ src/app/components/terminal-window/TerminalWindow.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-// import WindowComponent from "react-flexi-window";
 import { useRef, useState } from "react";
 import "./styles.css";
 import {
@@ -12,29 +11,12 @@ import {
 } from "../about-sections/GithubStats";
 
 export default function TerminalWindow() {
-  // const defaultWidth = 460;
-  // const defaultHeight = 320;
-  // const minimizedHeight = 52;
   const windowContainerRef = useRef<HTMLDivElement | null>(null);
   const [activeSection, setActiveSection] = useState(0);
   const { stats, loading, error } = useGithubStats();
   const info = ["General", "TopRepo", "Languages"];
-
-  // const [coordinates, setCoordinates] = useState<{
-  //   latitude: number | null;
-  //   longitude: number | null;
-  // }>({
-  //   latitude: null,
-  //   longitude: null,
-  // });
-  // const [error, setError] = useState<string | null>(null);
-  // const [loading, setLoading] = useState(false);
   const [minimize, setMinimize] = useState(false);
-  // const [windowPosition, setWindowPosition] = useState({ x: 50, y: 50 });
-  // const [windowSize, setWindowSize] = useState({
-  //   width: defaultWidth,
-  //   height: defaultHeight,
-  // });
+
 
   const captureWindowState = () => {
     const windowElement =
@@ -44,19 +26,11 @@ export default function TerminalWindow() {
       return;
     }
 
-    // const { width, height } = windowElement.getBoundingClientRect();
-    // // if (!minimize) {
-    // //   setWindowSize({ width, height });
-    // // }
-
     const transform = window.getComputedStyle(windowElement).transform;
 
     if (!transform || transform === "none") {
       return;
     }
-
-    // const matrix = new DOMMatrixReadOnly(transform);
-    // setWindowPosition({ x: matrix.m41, y: matrix.m42 });
   };
 
   const handleMinimize = () => {
@@ -64,80 +38,8 @@ export default function TerminalWindow() {
     setMinimize(!minimize);
   };
 
-  // const handleGetLocation = () => {
-  //   if (!navigator.geolocation) {
-  //     setError("Geolocation is not supported by your browser.");
-  //     return;
-  //   }
-  //   setLoading(true);
-  //   setError(null);
-  //   navigator.geolocation.getCurrentPosition(
-  //     (position) => {
-  //       setCoordinates({
-  //         latitude: position.coords.latitude,
-  //         longitude: position.coords.longitude,
-  //       });
-  //       setLoading(false);
-  //     },
-  //     (err) => {
-  //       setError(err.message);
-  //       setLoading(false);
-  //     },
-  //   );
-  // };
-
-  // useEffect(() => {
-  //   handleGetLocation();
-  // }, []);
 
   return (
-    // <div ref={windowContainerRef}>
-    //   <WindowComponent
-    //     key={minimize ? "minimized" : "expanded"}
-    //     boundary={true}
-    //     w={windowSize.width}
-    //     h={minimize ? minimizedHeight : windowSize.height}
-    //     minW={320}
-    //     minH={minimize ? minimizedHeight : 180}
-    //     x={windowPosition.x}
-    //     y={windowPosition.y}
-    //   >
-    //     <div className="flex h-full w-full flex-col bg-black/80">
-    //       <div className="flex flex-row justify-between border px-5 py-2 hover:cursor-move">
-    //         <p className="my-1 indent-0">SYSTEM STATUS</p>
-    //         <button onClick={handleMinimize} className="border-0 m-0 p-0">
-    //           <Image
-    //             src="/minimize.svg"
-    //             alt="Minimize button"
-    //             className="white w-5"
-    //             width={10}
-    //             height={10}
-    //           />
-    //         </button>
-    //       </div>
-    //       <div
-    //         className={
-    //           minimize
-    //             ? "h-0 overflow-hidden"
-    //             : "flex-1 overflow-y-auto border p-5"
-    //         }
-    //       >
-    //         <p className="indent-0">
-    //           CONNECTION: <span className="blinking-text">STABLE</span>
-    //         </p>
-    //         <p className="indent-0">
-    //           {/* LOCATION:{" "}
-    //           {coordinates.latitude && coordinates.longitude
-    //             ? `${coordinates.latitude}, ${coordinates.longitude}`
-    //             : "Not available"} */}
-    //           Location: Unknown
-    //         </p>
-    //         <p className="indent-0">TIME: </p>
-    //       <GithubStats />
-    //       </div>
-    //     </div>
-    //   </WindowComponent>
-    // </div>
     <div className="flex h-2/3 md:mx-5 md:w-1/3 w-full flex-col bg-black/80 overflow-hidden">
       <div className="flex flex-row justify-between border p-2">
         {info.map((section, index) => (
@@ -165,15 +67,6 @@ export default function TerminalWindow() {
           minimize ? "h-0 overflow-hidden" : "flex-1 overflow-y-auto border p-5"
         }
       >
-        {/* <p className="indent-0">
-          CONNECTION: <span className="blinking-text">STABLE</span>
-        </p> */}
-        <p className="indent-0">
-          {/* LOCATION:{" "}
-              {coordinates.latitude && coordinates.longitude
-                ? `${coordinates.latitude}, ${coordinates.longitude}`
-                : "Not available"} */}
-        </p>
         {activeSection === 0 && (
           <div>{stats && <GeneralInfo stats={stats} />}</div>
         )}
