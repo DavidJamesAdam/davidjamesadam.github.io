d72a851c71fa7b862604ec58ddf99c7397185364
DavidJamesAdam <davidjamesadam@gmail.com>
2026-06-30 17:04:27 -0600

    Trying to fix sidebar buttons

--- src/app/components/side-bar/SideBar.tsx
+++ src/app/components/side-bar/SideBar.tsx
@@ -17,21 +17,19 @@ export default function SideBar({
   isActive,
 }: SideBarProps) {
   return (
-    <div className="flex flex-row justify-start items-center">
-      <button
-        onClick={onClick}
-        className={`weathered-effect flex flex-col items-center justify-between text-[#894fad] hover:text-[#bc77e7] p-2 ${isActive ? "border rounded-md border-[#894fad] text-[#bc77e7]" : ""}`}
-      >
-        <Image
-          src={symbol}
-          className="w-12 hover:light-purple purple"
-          // className={`w-12 hover:light-purple ${isActive ? "light-purple" : "purple"}`}
-          alt={section}
-          width={10}
-          height={10}
-        />
-        <p>{section}</p>
-      </button>
-    </div>
+    <button
+      onClick={onClick}
+      className={`weathered-effect flex flex-col items-center justify-center text-[#894fad] hover:text-[#bc77e7] ${isActive ? "border rounded-md border-[#894fad] text-[#bc77e7]" : ""}`}
+    >
+      <Image
+        src={symbol}
+        alt={section}
+        width={70}
+        height={70}
+        className="purple"
+      />
+
+      <p>{section}</p>
+    </button>
   );
 }
